795e5aa4f426cf27b65e17b1adcf8d0e2a118d02
nmt3 <nmttkm3@gmail.com>
2023-05-28 22:21:23 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'invitations/index'
-    get 'invitations/show'
-    get 'invitations/edit'
-    get 'invitations/new'
-  end
-  namespace :public do
-    get 'invitations/index'
-    get 'invitations/show'
-    get 'invitations/edit'
-    get 'invitations/new'
-  end
-  devise_for :admins
-  devise_for :customers
+
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :public do
+    root to: 'homes#top'
+    resources :invitations, only[:new, :index, :show, :edit, :create, :update, :destory]
+  end
+
+  devise_for :admins,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get 'homes/top'
+    resources :invitations, only[:new, :index, :show, :edit, :create, :update, :destory]
+  end
+
 end
